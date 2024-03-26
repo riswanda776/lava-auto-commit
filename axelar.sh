3602b366491996e036fe8098af1e1615cba9d1ab
Riswanda Himawan <35683696+riswanda776@users.noreply.github.com>
2024-03-26 21:16:06 +0700

    Update axelar.sh

--- axelar.sh
+++ axelar.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-RPC_URL="https://tm.axelar.lava.build/lava-referer-8b51600b-b3cc-4893-89ec-5de41f0724db/"
+RPC_URL="https://tm.axelar.lava.build/lava-referer-b0f08f21-e5d6-4117-860d-f5de33f14a0b/"
 
 OUTPUT_DIR="logs"
 
